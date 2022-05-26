fc57653fd4605a95726e17c68ad06a8f9652e38f
yeonwoo <chiyy6162@gmail.com>
2022-05-26 15:35:09 +0900

    TIL: 클로져 및 콜백 비동기 연습

--- a_learning/promise/main.js
+++ a_learning/promise/main.js
@@ -103,3 +103,160 @@ function eat() {
 
 fruit(eat);
 eat();
+
+//클로져 예제
+
+function User() {
+  let _id = "연우";
+
+  const closures = {
+    getId: function () {
+      return _id;
+    },
+    setId: function (id) {
+      _id = id;
+    },
+  };
+  return closures;
+}
+
+const user = new User();
+
+console.log(user.getId());
+console.log(user._id);
+
+user.setId("후추");
+console.log(user.getId());
+console.log(user._id);
+
+// const timer = setTimeout(() => {
+//   console.log("setTimeout! : test stop!");
+// }, 3000);
+
+// function timeOut(callback) {
+//   setTimeout(() => {
+//     console.log("연우에용");
+//     callback();
+//   }, 3000);
+// }
+
+// timeOut(() => {
+//   console.log("내가 연우다");
+// });
+
+// console.log("0~100 랜덤숫자: ", Math.floor(Math.random() * 100) + 1);
+// //Example:
+// var arr2 = [1, 2, 4, 3, 5, 6, 8, 3, 5, 9, 10, 20, 50, 26];
+
+// // [].forEach(function(각요소하나씩, 순서){})
+// arr2.forEach(function (arg, idx) {
+//   console.log(idx, ":", arg, arr2[idx]);
+// });
+
+// // const numbers = [1, 2, 3, 4];
+
+// // //map
+// // const a = numbers.map(number => {
+// //   return number < 3;
+// // });
+
+// // console.log(a);
+// // // (4) [true, true, false, false]
+
+// // //filter
+
+// // const b = numbers.filter(number => {
+// //   return number < 3;
+// // });
+
+// // console.log(b);
+// // // (2) [1, 2]
+
+// const numbers = [1, 2, 3, 4];
+// const pushEl = numbers.push(3);
+// console.log(numbers); // (5) [1, 2, 3, 4, 3]
+// console.log(pushEl); // 5
+
+// const a = { a: 1 };
+// const b = { a: 1 };
+
+// const c = b;
+// console.log(c === b);
+
+// const fruits = ["Apple", "Banana", "Cherry"];
+// function toObject(aa, bb, cc) {
+//   return {
+//     a: aa,
+//     b: bb,
+//     c: cc,
+//   };
+// }
+
+// console.log(toObject(...fruits));
+// //깊은복사
+// const users = {
+//   name: "woo",
+//   age: 25,
+//   emails: ["chiyy6162@gmail.com"],
+// };
+
+// let textArr = JSON.stringify(users);
+// let copyArr = JSON.parse(textArr);
+
+// console.log("--------------------");
+// users.emails.push("pagemixpage@nate.com");
+// users.age = 31;
+
+// console.log(copyArr);
+// console.log(users);
+
+// const arr1 = [1, 2, 3, 4, 5];
+
+// const sum = arr1.reduce((stack, el) => {
+//   return stack + el;
+// }, 1);
+
+// console.log(sum);
+
+// let sum2 = 0;
+// arr1.forEach(function (el) {
+//   sum2 += el;
+// });
+
+// console.log(sum2);
+function a(callback) {
+  setTimeout(() => {
+    console.log("A");
+    callback();
+  }, 1000);
+}
+
+function b(callback) {
+  setTimeout(() => {
+    console.log("B");
+    callback();
+  }, 1000);
+}
+
+function c(callback) {
+  setTimeout(() => {
+    console.log("C");
+    callback();
+  }, 1000);
+}
+function d(callback) {
+  setTimeout(() => {
+    console.log("D");
+    callback();
+  }, 1000);
+}
+
+a(function () {
+  b(() => {
+    c(() => {
+      d(() => {
+        console.log("D끝남");
+      });
+    });
+  });
+});
