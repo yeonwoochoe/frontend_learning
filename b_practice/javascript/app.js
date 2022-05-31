603fef6ebfab02131bfa36a358d875a3fd1c3bf4
yeonwoo <chiyy6162@gmail.com>
2022-05-31 17:32:48 +0900

    TIL : username 반복부분 함수처리하고 유저 조건문걸기

--- b_practice/javascript/app.js
+++ b_practice/javascript/app.js
@@ -3,14 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //로컬스토리지에 유저정보가 없을 경우
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //hello 유저네임 보여주기
+  paintGreetings(savedUsername);
+}
