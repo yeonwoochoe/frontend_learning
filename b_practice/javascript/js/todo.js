dbebe46205ee41f03ef18b0adfd7723809deb6f5
yeonwoo <chiyy6162@gmail.com>
2022-06-03 17:45:34 +0900

    todolist 이전 값과 새로운 값 둘 다 저장하기.

--- b_practice/javascript/js/todo.js
+++ b_practice/javascript/js/todo.js
@@ -2,6 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+//키 변수화 하기
+let toDos = [];
+//빈 배열 생성
+//기존 const를 let으로 변경하여 업데이트 할 수 있도록 바꿔주기
+
+//로컬스토리지에 저장
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  //stringify이용하여 텍스트로 변환하여 저장해주기
+}
+
 //삭제 기능 함수
 function deleteToDo(event) {
   const li = event.target.parentElement;
@@ -29,7 +41,23 @@ function handleToDoSubmit(event) {
   const newTodo = toDoInput.value;
   //인풋창비워주기
   toDoInput.value = "";
+  toDos.push(newTodo);
+  //인풋값 빈배열 todos에 넣어주기
   paintToDo(newTodo);
+  //인풋값 화면에 그려주기
+  saveToDos();
 }
-
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+//가져오기
+
+if (savedToDos !== null) {
+  //로컬스토리지에 저장이 되어있다면 (null이 아니라면)
+  const parsedToDos = JSON.parse(savedToDos);
+  //다시 가져와서 자바스크립트로 배열로 변환하기
+  toDos = parsedToDos;
+  //toDos에 parsedToDos를 넣어서 전에 있던 toDo 복원해주기
+  parsedToDos.forEach(paintToDo);
+  //각각 아이템별로 forEach 돌리기
+}
